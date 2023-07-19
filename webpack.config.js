80707d7338f9029c4aac4a0a48a0b71698fdec0c
Porkenvolf <89871231+porkenvolf@users.noreply.github.com>
2023-07-19 17:30:12 -0300

    Work on first mockup

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     devtool: "inline-source-map",
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Burger Time!",
+            title: "ToDo List",
         }),
     ],
     output: {
